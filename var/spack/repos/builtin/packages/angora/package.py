486d22ce03a9d70b76682413a6bc5b459d3d4866
sajid <sajidsyed2021@u.northwestern.edu>
2020-03-13 15:46:51 -0400

    update

--- var/spack/repos/builtin/packages/angora/package.py
+++ var/spack/repos/builtin/packages/angora/package.py
@@ -15,16 +15,28 @@ class Angora(AutotoolsPackage):
 
     version('0.22.5', sha256='f95e4a8a7a570f5c4e093ec04fdf89ee352a4cbd4f3f4d36a288f3950d2af602')
 
-    variant('mpi', default=True,
-            description="Enable MPI support.")
-
     depends_on('libconfig')
     depends_on('blitz')
     depends_on('boost')
     depends_on('hdf5')
+    depends_on('mpi')
 
     def configure_args(self):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
         args = []
+        
+        args.append('--with-mpi=%s' %
+                    self.spec['mpi'].prefix.lib)
+
+        args.append('--with-hdf5=%s' %
+                    self.spec['hdf5'].prefix.lib)
+
+        args.append('--with-blitz=%s' %
+                    self.spec['blitz'].prefix.lib)
+
+        args.append('--with-boost=%s' %
+                    self.spec['boost'].prefix.lib)
+
+        args.append('--with-config=%s' %
+                    self.spec['libconfig'].prefix.lib)
+
         return args
